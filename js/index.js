cdd17b6f0b575e9f98ebc0a2b7e68a20af946431
Sameed1990 <111309698+Sameed1990@users.noreply.github.com>
2022-08-21 06:45:54 -0700

    summary

--- js/index.js
+++ js/index.js
@@ -8,14 +8,4 @@ function CloseModal(){
 }
 
 
-(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items:3,
-  //      autoplay:false,
-        margin:30,
-        loop:true,
-        dots:true
-  //      nav:true,
-  //      navText:["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>" ]
-    });
-  });
+// AOS.init();
